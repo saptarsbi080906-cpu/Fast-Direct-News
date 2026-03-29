ca1b1c61291d3429c52ec62f505047d94f20ec6b
[Sapatarshi Sarkar] <saptarsbi080906@gmail.com>
2026-03-29 13:24:46 +0530

    Fixed Web Crashing

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -12,16 +12,21 @@ export default class Newscomponent extends Component {
   }
   async componentDidMount() {
     let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=1cbf1709b4f8418ead26050a190e7a2d&category=${this.props.category}`;
+
     let data = await fetch(url);
     let parsedData = await data.json();
-    this.setState({ articles: parsedData.articles });
+    console.log(parsedData);
+
+    this.setState({
+      articles: parsedData.articles || [],
+    });
   }
   render() {
     return (
       <div className="container my-3">
         <h2>Fast & Direct News - Top Headlines</h2>
         <div className="row">
-          {this.state.articles.map((element) => {
+          {(this.state.articles || []).map((element) => {
             return (
               <div className="col-md-4 my-3" key={element.url}>
                 <Newsitem
