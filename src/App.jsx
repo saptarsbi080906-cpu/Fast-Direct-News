bf7da57125fb65a4a3359243c529a45fea11c7bd
[Sapatarshi Sarkar] <saptarsbi080906@gmail.com>
2026-03-29 13:04:12 +0530

    Bug Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
-import Newscomponent from "./components/News";
+import News from "./components/News";
 import { Routes, Route } from "react-router-dom";
 import About from "./components/About";
 
@@ -10,44 +10,8 @@ export default class App extends Component {
       <>
         <Navbar />
         <Routes>
-          <Route
-            exact
-            path="/"
-            element={<Newscomponent key="general" category="general" />}
-          />
-          <Route
-            exact
-            path="/business"
-            element={<Newscomponent key="business" category="business" />}
-          />
-          <Route
-            exact
-            path="/entertainment"
-            element={
-              <Newscomponent key="entertainment" category="entertainment" />
-            }
-          />
-          <Route
-            exact
-            path="/health"
-            element={<Newscomponent key="health" category="health" />}
-          />
-          <Route
-            exact
-            path="/science"
-            element={<Newscomponent key="science" category="science" />}
-          />
-          <Route
-            exact
-            path="/sports"
-            element={<Newscomponent key="sports" category="sports" />}
-          />
-          <Route
-            exact
-            path="/technology"
-            element={<Newscomponent key="technology" category="technology" />}
-          />
-          <Route exact path="/about" element={<About />} />
+          <Route path="/" element={<News key="general" category="general" />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </>
     );
